d264fdfd8995dc49dbe463d3983a224baccb0f7e
Carlos Romao <cvpromao@gmail.com>
2024-02-07 16:18:04 -0300

    Add project files.

---
+++ src/RocketsetAuction.API/Repositories/RocketseatAuctionDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+
+namespace RocketseatAuction.API.Repositories
+{
+    public class RocketseatAuctionDbContext : DbContext
+    {
+        public DbSet<Auction> Auctions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Offer> Offers { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=D:\\Users\\CarlosRomao\\Videos\\NLW-C#\\leilaoDbNLW.db");
+        }
+    }
+}
